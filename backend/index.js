0f18eb34af9bdcc644a6ad3b58c20d11ac8baeaf
Zyrus-47 <140943539+Zyrus-47@users.noreply.github.com>
2025-06-04 19:36:26 +0530

    installed dependencies and created backend folder

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app =express();
+
+const server = require('http').createServer(app);
+const {Server} =require ("socket.io");
+
+const io= new Server(server);
+
+app.get("/",(req,res)=>{
+    res.send("this is project SLaiTE");
+});
+
+io.on("connection",(socket)=>{
+    console.log("User connected");
+});
+
+
+
+
+const port =process.env.PORT || 5000;
+server.listen(port,()=> console.log("server is running on local host 5000"))
